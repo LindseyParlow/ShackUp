b5faa4af0d5ef2fcc7a9289feb18d9b799ef18a4
Lindsey Parlow <lmptipper@hotmail.com>
2018-03-16 01:17:58 -0500

    more ado

--- Scripts/DbReset.sql
+++ Scripts/DbReset.sql
@@ -8,10 +8,34 @@ go
 
 create procedure DbReset as
 begin
+	delete from Listings
 	delete from States;
+	delete from BathroomTypes;
+	delete from AspNetUsers where id = '00000000-0000-0000-0000-000000000000';
+
 
 	insert into States(StateId, StateName)
 	values ('OH', 'Ohio'),
 	('KY', 'Kentucky'),
 	('MN', 'Minnesota');
+
+	set identity_insert BathroomTypes on;
+
+	insert into BathroomTypes (BathroomTypeId, BathroomTypeName)
+	values (1, 'Indoor'),
+	(2, 'Outdoor'),
+	(3, 'None')
+
+	set identity_insert BathroomTypes off;
+
+	insert into AspNetUsers(Id, EmailConfirmed, PhoneNumberConfirmed, Email, StateId, TwoFactorEnabled, LockoutEnabled, AccessFailedCount, UserName)
+	values('00000000-0000-0000-0000-000000000000', 0, 0, 'test@test.com', 'OH', 0, 0, 0, 'test');
+
+	set identity_insert Listings on;
+
+	insert into Listings(ListingId, UserId, StateId, BathroomTypeId, Nickname, City, Rate, SquareFootage, HasElectric, HasHeat, ImageFileName)
+	values(1, '00000000-0000-0000-0000-000000000000', 'OH', 3, 'Test shack 1', 'Cleveland', 120, 400, 0, 1, 'placeholder.png')
+
+	set identity_insert Listings off;
+
 end
--- Scripts/Sprocs.sql
+++ Scripts/Sprocs.sql
@@ -23,5 +23,86 @@ begin
 	order by BathroomTypeName
 
 end
+go
 
+if exists(select * from INFORMATION_SCHEMA.ROUTINES
+	where ROUTINE_NAME = 'ListingsInsert')
+		drop procedure ListingsInsert
+go
+
+create procedure ListingsInsert (
+	@ListingsId int output,
+	@UserId nvarchar(128),
+	@StateId char(2),
+	@BathroomTypeId int,
+	@Nickname nvarchar(50),
+	@City nvarchar(50),
+	@Rate decimal(7,2),
+	@SquareFootage decimal(7,2),
+	@HasElectric bit,
+	@HasHeat bit,
+	@ImageFileName varchar(50)
+) as
+begin
+	insert into Listings(UserId, StateId, BathroomTypeId, Nickname, City, Rate, SquareFootage, HasElectric, 
+		HasHeat, ImageFileName)
+	values (@UserId, @StateId, @BathroomTypeId, @Nickname, @City, @Rate, @SquareFootage, @HasElectric, @HasHeat, @ImageFileName);
+
+	set @ListingsId = SCOPE_IDENTITY();
+
+end
+go
+
+if exists(select * from INFORMATION_SCHEMA.ROUTINES
+	where ROUTINE_NAME = 'ListingsUpdate')
+		drop procedure ListingsUpdate
+go
+
+create procedure ListingsUpdate (
+	@ListingsId int,
+	@UserId nvarchar(128),
+	@StateId char(2),
+	@BathroomTypeId int,
+	@Nickname nvarchar(50),
+	@City nvarchar(50),
+	@Rate decimal(7,2),
+	@SquareFootage decimal(7,2),
+	@HasElectric bit,
+	@HasHeat bit,
+	@ImageFileName varchar(50)
+) as
+begin
+	update Listings set 
+		UserId = @UserId, 
+		StateId = @StateId, 
+		BathroomTypeId = @BathroomTypeId, 
+		Nickname = @Nickname, 
+		City = @City, 
+		Rate = @Rate, 
+		SquareFootage = @SquareFootage, 
+		HasElectric = @HasElectric, 
+		HasHeat = @HasHeat, 
+		ImageFileName = @ImageFileName
+	where ListingId = @ListingsId
+
+end
+go
+
+if exists(select * from INFORMATION_SCHEMA.ROUTINES
+	where ROUTINE_NAME = 'ListingsDelete')
+		drop procedure ListingsDelete
+go
+
+create procedure ListingsDelete (
+	@ListingsId int
+)as 
+begin
+	begin transaction
+
+	delete from Contacts where ListingId = @ListingsId;
+	delete from Favorites where ListingId = @ListingsId;
+	delete from Listings where ListingId = @ListingsId;
+
+	commit transaction
+end
 go
